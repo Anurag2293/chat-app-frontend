413a3dacc358521d404ad7134a51cfd93a968820
Anurag Dhote <anuragdhote392@gmail.com>
2024-08-13 23:48:02 +0530

    feat - chat prototype complete

--- src/components/chat-room.tsx
+++ src/components/chat-room.tsx
@@ -1,12 +1,26 @@
-
 "use client"
 
 import { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
 
 import PhoneIcon from "@/components/icons/phone";
 import SendIcon from "@/components/icons/send";
@@ -14,7 +28,7 @@ import VideoIcon from "@/components/icons/video";
 
 import { useSocketStore } from "@/providers/socket-store-provider";
 
-const SelfChat = ({ children }: { children: React.ReactNode}) => {
+const SelfChat = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="flex w-max max-w-[65%] flex-col gap-2 rounded-full px-4 py-2 text-sm ml-auto bg-primary text-primary-foreground">
             {children}
@@ -22,7 +36,7 @@ const SelfChat = ({ children }: { children: React.ReactNode}) => {
     )
 }
 
-const OtherChat = ({ children }: { children: React.ReactNode}) => {
+const OtherChat = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="flex w-max max-w-[65%] flex-col gap-2 rounded-full px-4 py-2 text-sm bg-muted">
             {children}
@@ -30,29 +44,62 @@ const OtherChat = ({ children }: { children: React.ReactNode}) => {
     )
 }
 
-type ChatType = { senderID: string, text: string }
+const chatFormSchema = z.object({
+    chatMessage: z.string().min(1, {
+        message: "Enter a valid room name.",
+    }),
+});
 
-// const chatHistory: ChatType[] = []
+
+type ChatType = { senderID: string, text: string }
 
 export default function ChatRoom() {
     const router = useRouter();
     const { roomID } = useParams();
+
+    const { toast } = useToast();
+    const form = useForm<z.infer<typeof chatFormSchema>>({
+        resolver: zodResolver(chatFormSchema),
+        defaultValues: {
+            chatMessage: "",
+        },
+    });
+
     const { socket } = useSocketStore(s => s);
     const [chatHistory, setChatHistory] = useState<ChatType[]>([]);
 
     useEffect(() => {
-        if (socket) {
-            console.log("Socket connection on : ", socket.id);
+        const joinSocketRoom = () => {
+            if (socket) {
+                console.log("Socket connection on : ", socket.id);
 
-            socket.emit("joined-room", roomID);
+                socket.emit("joined-room", roomID);
 
-            socket.on("join-message", (joinChat: ChatType) => {
-                setChatHistory(p => [...p, joinChat]);
-            })
-        } else {
-            router.push("/"); 
+                socket.on("join-message", (joinChat: ChatType) => {
+                    setChatHistory(p => [...p, joinChat]);
+                });
+
+                socket.on("emit-message", (message: ChatType) => {
+                    setChatHistory(p => [...p, message]);
+                });
+            } else {
+                router.push("/");
+            }
         }
+
+        joinSocketRoom();
     }, []);
+
+    const onSubmit = (values: z.infer<typeof chatFormSchema>) => {
+        if (!socket) {
+            return toast({
+                variant: "destructive",
+                title: "Socket connection doesn't exists!",
+                action: <ToastAction altText="Try again">Try again</ToastAction>,
+            });
+        }
+        socket.emit("send-message", roomID, values.chatMessage);
+    }
 
     return (
         <>
@@ -63,7 +110,7 @@ export default function ChatRoom() {
                         <AvatarFallback>OM</AvatarFallback>
                     </Avatar>
                     <div className="grid gap-0.5">
-                        <p className="text-sm font-medium leading-none">Sofia Davis: {socket?.id}</p>
+                        <p className="text-sm font-medium leading-none">{roomID}</p>
                         <p className="text-xs text-muted-foreground">Active 2h ago</p>
                     </div>
                 </div>
@@ -85,27 +132,28 @@ export default function ChatRoom() {
                     }
                     return <OtherChat>{chat.senderID}: {chat.text}</OtherChat>
                 })}
-                {/* <SelfChat>
-                    Hey hope you&apos;re doing well! We should catch up sometime soon. 🙏
-                </SelfChat>
-                <OtherChat>
-                    Sure! I&apos;m free this weekend if you want to grab a coffee.
-                </OtherChat>
-                <SelfChat>
-                    Sounds good! Let&apos;s meet at the Starbucks on 5th Ave.
-                </SelfChat>
-                <OtherChat>
-                    I&apos;ll message you on Saturday.
-                </OtherChat> */}
             </div>
             <div className="border-t">
-                <form className="flex w-full items-center space-x-2 p-3">
-                    <Input id="message" placeholder="Type your message..." className="flex-1" autoComplete="off" />
-                    <Button type="submit" size="icon">
-                        <span className="sr-only">Send</span>
-                        <SendIcon className="h-4 w-4" />
-                    </Button>
-                </form>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full items-center space-x-2 p-3">
+                        <FormField
+                            control={form.control}
+                            name="chatMessage"
+                            render={({ field }) => (
+                                <FormItem className="flex-1">
+                                    <FormControl autoFocus>
+                                        <Input placeholder="Type your message..." {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" size="icon">
+                            <span className="sr-only">Send</span>
+                            <SendIcon className="h-4 w-4" />
+                        </Button>
+                    </form>
+                </Form>
             </div>
         </>
     )
