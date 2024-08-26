e79a8ae54bcf2740de4c61c4f78fefb223d6f2aa
Anurag Dhote <anuragdhote392@gmail.com>
2024-08-26 14:07:27 +0530

    chore - socket store reset

--- src/stores/socket-store.ts
+++ src/stores/socket-store.ts
@@ -1,21 +1,20 @@
-import type { Socket } from "socket.io-client";
-import { createStore } from "zustand";
-import { io } from "socket.io-client";
 import { useRouter } from "next/navigation";
+import { Message } from "@prisma/client";
+import { io, type Socket } from "socket.io-client";
+import { createStore } from "zustand";
 
-import { ChatType } from "@/types/socketTypes";
 import { useToast } from "@/components/ui/use-toast";
 
 export type SocketState = {
   socket: Socket | null;
-  messages: ChatType[];
+  messages: Message[];
 };
 
 export type SocketActions = {
   setSocket: (newSocket: Socket) => void;
-  joinSocketRoom: (roomID: string) => void;
-  confirmJoinRoom: (roomID: string) => void;
-  sendMessageToRoom: (roomID: string, chatMessage: string) => void;
+  // joinSocketRoom: (roomID: string) => void;
+  // confirmJoinRoom: (roomID: string) => void;
+  // sendMessageToRoom: (roomID: string, chatMessage: string) => void;
 };
 
 export type SocketStore = SocketState & SocketActions;
@@ -30,37 +29,33 @@ export const defaultSocketInitState: SocketState = {
 };
 
 export const createSocketStore = (
-  initState: SocketState = defaultSocketInitState,
+  initState: SocketState = defaultSocketInitState
 ) => {
+  const socket = io("http://localhost:8080", {
+    auth: {
+      email: ""
+    }
+  });
+  const { toast } = useToast();
+  const router = useRouter();
+
+  socket.on("connect", () => {
+    console.log("Connected to server: ", socket.id);
+    return toast({
+      description: "Connected to server: " + socket.id,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected to server: ", socket.id);
+    return toast({
+      variant: "destructive",
+      description: "Disconnected from server.",
+    });
+  });
+
   return createStore<SocketStore>()((set) => {
-    const socket = io("http://localhost:8080");
-    const { toast } = useToast();
-    const router = useRouter();
-
-    socket.on("connect", () => {
-      console.log("Connected to server: ", socket.id);
-      return toast({
-        description: "Connected to server: " + socket.id,
-      });
-    });
-
-    socket.on("disconnect", () => {
-      console.log("Disconnected to server: ", socket.id);
-      return toast({
-        variant: "destructive",
-        description: "Disconnected from server.",
-      });
-    });
-
-    socket.on("join-message", (joinMessage: ChatType) => {
-      console.log("Join Message!");
-      set((state) => ({
-        ...state,
-        messages: [...state.messages, joinMessage],
-      }));
-    });
-
-    socket.on("emit-message", (message: ChatType) => {
+    socket.on("emit-message", (message: Message) => {
       console.log("Emit Message!");
       set((state) => ({
         ...state,
@@ -78,47 +73,48 @@ export const createSocketStore = (
           socket: newSocket,
         }));
       },
-
-      joinSocketRoom: (roomID: string) => {
-        if (!socket) {
-          return toast({
-            variant: "destructive",
-            title: "Socket connection doesn't exists",
-            // action: <ToastAction altText="Try again">Try again</ToastAction>,
-          });
-        }
-        socket.emit("join-room", roomID, (error: string) => {
-          if (error) {
-            return toast({
-              variant: "destructive",
-              title: "Error joining room!",
-              description: error,
-            });
-          }
-          // router.push(`/chat/${roomID}`);
-          socket.emit("joined-room", roomID);
-          toast({
-            description: "Joined room successfully!",
-          });
-        });
-      },
-
-      confirmJoinRoom: (roomID: string) => {
-        socket.emit("joined-room", roomID);
-      },
-
-      sendMessageToRoom: (roomID: string, chatMessage: string) => {
-        if (chatMessage.trim().length === 0) {
-          return;
-        }
-        if (!socket) {
-          return toast({
-            variant: "destructive",
-            title: "Socket connection doesn't exists!",
-          });
-        }
-        socket.emit("send-message", roomID, chatMessage);
-      },
     };
   });
 };
+
+/*
+joinSocketRoom: (roomID: string) => {
+  if (!socket) {
+    return toast({
+      variant: "destructive",
+      title: "Socket connection doesn't exists",
+    });
+  }
+  socket.emit("join-room", roomID, (error: string) => {
+    if (error) {
+      return toast({
+        variant: "destructive",
+        title: "Error joining room!",
+        description: error,
+      });
+    }
+    // router.push(`/chat/${roomID}`);
+    socket.emit("joined-room", roomID);
+    toast({
+      description: "Joined room successfully!",
+    });
+  });
+},
+
+confirmJoinRoom: (roomID: string) => {
+  socket.emit("joined-room", roomID);
+},
+
+sendMessageToRoom: (roomID: string, chatMessage: string) => {
+  if (chatMessage.trim().length === 0) {
+    return;
+  }
+  if (!socket) {
+    return toast({
+      variant: "destructive",
+      title: "Socket connection doesn't exists!",
+    });
+  }
+  socket.emit("send-message", roomID, chatMessage);
+},
+*/
--- src/types/socketTypes.ts
+++
@@ -1,1 +0,0 @@
-export type ChatType = { senderID: string; text: string };
